1a6b90779089d08e15330a71188d0b7449c56ab1
LillaV <Lilla.Vrencian@msg.group>
2023-08-11 16:44:47 +0300

    "tokenAccess" name is changed into token.

--- angular-demo/src/app/login/models/login-response.ts
+++ angular-demo/src/app/login/models/login-response.ts
@@ -1,6 +1,6 @@
 export class LoginResponse{
   constructor(
-    public accessToken: string,
+    public token: string,
     public id: number,
     public username: string,
     public email: string,
