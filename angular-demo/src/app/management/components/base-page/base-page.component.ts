b30d6494ec8244e2c8067fb15a4abec7063c4a65
Noemi Veres <Noemi.Veres@msg.group>
2023-08-25 15:40:30 +0200

    number of new notifications

--- angular-demo/src/app/management/components/base-page/base-page.component.ts
+++ angular-demo/src/app/management/components/base-page/base-page.component.ts
@@ -14,6 +14,7 @@ import {NotificationService} from "../../services/notification.service";
 export class BasePageComponent implements OnInit{
 
   notifoication:Notif[] = [];
+  length = 0;
 
   constructor(private router:Router,private service:LogoutService,private notificationService:NotificationService,private websocketService:WebSocketService,private translocoService:TranslocoService) { }
 
@@ -41,9 +42,17 @@ export class BasePageComponent implements OnInit{
         this.notifoication = notifications
       }
     )
+    this.websocketService.notificationEvent.subscribe(
+      (notification) => {
+        this.length++;
+        this.notifoication.splice(0,1 )
+        this.notifoication.push(notification)
+      }
+    )
   }
 
   seeAllNotifications() {
+    this.length = 0;
     this.router.navigate(["management/notifications"])
   }
   toggleMenu() {
@@ -64,9 +73,6 @@ export class BasePageComponent implements OnInit{
     }
     if(page === 'campaigns'){
       this.router.navigate(["/management/campaigns/list"]);
-    }
-    if(page === 'donations'){
-      this.router.navigate(["/management/donations/list"])
     }
     if(page === 'home'){
       this.router.navigate(["/management/home"]);
--- angular-demo/src/app/management/services/web-socket.service.ts
+++ angular-demo/src/app/management/services/web-socket.service.ts
@@ -1,6 +1,8 @@
-import {Injectable} from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
 import { Client } from "@stomp/stompjs"
 import {ToastrService} from "ngx-toastr";
+import {BehaviorSubject} from "rxjs";
+import {Notif} from "../model/notification";
 
 const CHAT_URL = "ws://localhost:8080/api/websocket";
 @Injectable({
@@ -9,7 +11,7 @@ const CHAT_URL = "ws://localhost:8080/api/websocket";
 export class WebSocketService {
 
   client
-
+  notificationEvent = new EventEmitter<Notif>();
   constructor(private toastr:ToastrService) {
     this.client = new Client({
       brokerURL: "ws://localhost:8080/api/websocket",
@@ -17,6 +19,7 @@ export class WebSocketService {
         let permissions: string[] = JSON.parse(localStorage.getItem("permissions") as string);
           this.client.subscribe("/topic/userManagement", message => {
             const notification = JSON.parse(message.body);
+            this.notificationEvent.emit(notification)
             toastr.success(notification.text, notification.date);
           })
         }
